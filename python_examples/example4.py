b5410ef7b02019c346d278c1798a45437c2d21e4
Matt Butterfield <mbutterfield@onshift.com>
2014-03-24 21:05:02 -0400

    add population class

--- python_examples/example4.py
+++ python_examples/example4.py
@@ -5,6 +5,8 @@ http://burakkanber.com/blog/machine-learning-genetic-algorithms-in-javascript-pa
 
 import sys
 import random
+import math
+import time
 from elements import elements
 
 
@@ -58,8 +60,83 @@ class Chromosome(object):
         return [child1, child2]
 
 
+class Population(object):
+    def __init__(self, size=20, elements=elements):
+        self.size = size
+        self.elements = elements
+        self.elitism = 0.2
+        self.chromosomes = []
+        self.fill()
+
+    def fill(self):
+        while len(self.chromosomes) < self.size:
+            if len(self.chromosomes) < self.size / 3:
+                self.chromosomes.append(Chromosome(self.elements))
+            else:
+                self.mate()
+
+    def sort(self):
+        self.chromosomes.sort(key=lambda x: x.calc_score())
+
+    def kill(self):
+        target = math.floor(self.elitism * len(self.chromosomes))
+        while len(self.chromosomes) > target:
+            self.chromosomes.pop()
+
+    def mate(self):
+        key1 = self.chromosomes[random.randint(0, len(self.chromosomes) - 1)]
+        key2 = key1
+        while key2 == key1:
+            key2 = self.chromosomes[random.randint(0, len(self.chromosomes) - 1)]
+        children = key1.mate_with(key2)
+        self.chromosomes += children
+
+    def generation(self):
+        self.sort()
+        self.kill()
+        self.mate()
+        self.fill()
+        self.sort()
+
+    def run(self, threshold, no_improvement, last_score, i):
+        if not threshold:
+            threshold = 1000
+        if not no_improvement:
+            no_improvement = 0
+        if not last_score:
+            last_score = False
+        if not i:
+            i = 0
+
+        if no_improvement < threshold:
+            last_score = self.chromosomes[0].calc_score()
+            self.generation()
+
+            if last_score >= self.chromosomes[0].calc_score():
+                no_improvement += 1
+            else:
+                no_improvement = 0
+
+            i += 1
+
+            if i % 10 == 0:
+                self.display(i, no_improvement)
+            time.sleep(0.1)
+            self.run(threshold, no_improvement, last_score, i)
+            return False
+
+        self.display(i, no_improvement)
+
+    def display(self, i, no_improvement):
+        print "Generation:\t" + str(i)
+        print "Best Value:\t" + str(self.chromosomes[0].score)
+        print "Weight:\t"+ str(self.chromosomes[0].weight)
+        print "No change in:\t" + str(no_improvement)
+
+
 def main():
-    c1 = Chromosome(elements)
+    p = Population()
+    p.run()
 
 if __name__ == '__main__':
     sys.exit(main())
